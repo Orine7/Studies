dbbda950c47a96932c038b00d956a5549b615143
Eduardo <eduardofilho_08@hotmail.com>
2021-03-24 11:46:36 -0300

    selenium classification

--- small_projects/webscraping.py
+++ small_projects/webscraping.py
@@ -1,11 +1,20 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox(executable_path='/home/orine/Desktop/geckodriver')
+class WebAutomaton(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Firefox(executable_path='/home/orine/Desktop/geckodriver')
+    def searchSquares(self, search_site, search_element):
+        driver = self.driver
+        driver.get(search_site)
+        elem = driver.find_element_by_name(search_element)
+        elem.send_keys("Square")
+        elem.send_keys(Keys.RETURN)
+    def closeDown(self):
+        self.driver.close()
 
-driver.get("https://copyassignment.com/")
-
-search_element = driver.find_element_by_id("is-search-input-0")
-
-text = "Turtle"
-search_element.send_keys(text)
+if __name__ == "__main__":
+    automaton = WebAutomaton()
+    automaton.setUp()
+    automaton.searchSquares(search_site="https://www.youtube.com/", search_element= "search_query")
